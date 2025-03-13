1d64e452e0c276d6ae9cfd913f628023ecffb4d4
Brunno Mota <soma.bsm@gmail.com>
2025-03-13 17:07:41 -0300

    lets finish this thing pleaaaase

--- bsmsomaReactLandingPage/app/routes/projects.tsx
+++ bsmsomaReactLandingPage/app/routes/projects.tsx
@@ -6,30 +6,41 @@ import {
     SiTypescript,
     SiTailwindcss,
     SiMysql,
-    SiReactrouter 
+    SiReactrouter,
+    SiHtml5,
+    SiCss3,
+    SiJavascript,
+    SiSass
 } from "react-icons/si";
+import rmcImage from "../assets/image/exemplosite.png" //temporario
+import joaoVitorImage from "../assets/image/exemplosite2.png" //temporario
+
+//todo: adicionar imagens dos projetos
 
 interface ProjectCardProps {
     title: string;
+    type: string;
     description: string;
     technologies: JSX.Element[];
     link: string;
+    image: string;
 }
 
-const ProjectCard = ({ title, description, technologies, link }: ProjectCardProps) => {
+const ProjectCard = ({ title, type, description, technologies, link, image }: ProjectCardProps) => {
     const { theme } = useTheme();
     
     return (
-        <a href={link} target="_blank" rel="noopener noreferrer" 
-           className={`project-card ${theme}`}>
+        <div className={`project-card ${theme}`}>
+            <img src={image} alt={`preview do projeto ${title}`} />
             <h3>{title}</h3>
+            <h4>{type}</h4>
             <p>{description}</p>
             <div className="tech-stack">
                 {technologies.map((tech, index) => (
                     <span key={index}>{tech}</span>
                 ))}
             </div>
-        </a>
+        </div>
     );
 };
 
@@ -48,16 +59,27 @@ export default function Projects() {
 
     const projectsData = [
         {
-            title: "Landing Page",
-            description: "Portfolio pessoal desenvolvido com React e TypeScript",
-            technologies: [<SiReact />, <SiTypescript />, <SiTailwindcss />],
-            link: "https://github.com/seu-usuario/projeto1"
+            title: "Requinte - Locação de Trajes",
+            type: "Catalogo Virtual",
+            description: "Catalogo virtual de trajes para locação, desenvolvido com React, React Router, Sass, Firebase",
+            technologies: [<SiReact />, <SiSass />, <SiFirebase />, <SiReactrouter />],
+            link: "https://github.com/seu-usuario/projeto1",
         },
         {
-            title: "E-commerce",
+            title: "RMC Maquinas",
+            type: "Institucional",
+            description: "Site institucional otimizado para SEO, desenvolvido com foco em conversão para atrair clientes locais e de outras regiões.",
+            technologies: [<SiHtml5 />, <SiCss3 />, <SiJavascript />],
+            link: "https://github.com/seu-usuario/projeto1",
+            image: rmcImage
+        },
+        {
+            title: "João Vitor - Fotografia",
+            type: "Landing Page",
             description: "Loja virtual com integração ao Firebase",
-            technologies: [<SiReact />, <SiFirebase />, <SiReactrouter />],
-            link: "https://github.com/seu-usuario/projeto2"
+            technologies: [<SiReact />, <SiTailwindcss />, <SiReactrouter />],
+            link: "https://github.com/seu-usuario/projeto2",
+            image: joaoVitorImage
         },
         // Adicione mais projetos conforme necessário
     ];
